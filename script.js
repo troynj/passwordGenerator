b53622dd68ceb731c9bf149931cd45f677c0ceaf
Troy Johnson <troynjohnson6@gmail.com>
2022-12-21 09:58:00 -0800

    Adding logic to prompt pw length

--- script.js
+++ script.js
@@ -3,13 +3,26 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  console.log("Entered wpFunction");
+  //var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  var pwLengthCheck = false;
+  var pwLength;
+  while (!pwLengthCheck) {
+    pwLength = prompt(
+      "How many characters would you like your password to be? Enter a number between 8 and 128"
+    );
 
+    if (pwLength < 8) {
+      alert("Password must be atleast 8 characters long");
+    } else if (pwLength > 128) {
+      alert("Password cannot be longer than 128 characters");
+    } else pwLengthCheck = true;
+  }
 }
+
+passwordText.value = password;
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
