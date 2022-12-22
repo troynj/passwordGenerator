19b3a1275af5355cf8ce2b6559d5330fff9ca473
Troy Johnson <troynjohnson6@gmail.com>
2022-12-22 08:57:01 -0800

    reverting and updating length variable name

--- script.js
+++ script.js
@@ -1,126 +1,74 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var pwObj = [
-  {//0
-    type: "lowercase",
-    desired: null,
-    options: [
-      "a",
-      "b",
-      "c",
-      "d",
-      "e",
-      "f",
-      "g",
-      "h",
-      "i",
-      "j",
-      "k",
-      "l",
-      "m",
-      "n",
-      "o",
-      "p",
-      "q",
-      "r",
-      "s",
-      "t",
-      "u",
-      "v",
-      "w",
-      "x",
-      "y",
-      "z",
-    ],
-    generateChar: function () {
-      var char = Math.floor(Math.random() * 26);
-      return availableCriteria.aplha[char].toLowerCase();
-    },
+var pwOptions = {
+  lowercase: null,
+  uppercase: null,
+  numeric: null,
+  special: null,
+  pwLength: null,
+  exclude: null,
+};
+var availableCriteria = {
+  aplha: [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ],
+  //num: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
+  symbol: {
+    "!": true,
+    "@": true,
+    "#": true,
+    "$": true,
+    "%": true,
+    "^": true,
+    "&": true,
+    "*": true,
+    "(": true,
+    ")": true,
+    "-": true,
+    "=": true,
+    "_": true,
+    "+": true,
+    "[": true,
+    "]": true,
+    "{": true,
+    "}": true,
+    "|": true,
+    ":": true,
+    ";": true,
+    ",": true,
+    ".": true,
+    "<": true,
+    ">": true,
+    "?": true,
+    "/": true,
   },
-  {//1
-    type: "uppercase",
-    desired: null,
-    options: [
-      "A",
-      "B",
-      "C",
-      "D",
-      "E",
-      "F",
-      "G",
-      "H",
-      "I",
-      "J",
-      "K",
-      "L",
-      "M",
-      "N",
-      "O",
-      "P",
-      "Q",
-      "R",
-      "S",
-      "T",
-      "U",
-      "V",
-      "W",
-      "X",
-      "Y",
-      "Z",
-    ],
-  },
-  {//2
-    type: "numeric",
-    desired: null,
-    options: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
-    generateChar: function () {
-      return Math.floor(Math.random() * 10);
-    },
-  },
-  {//3
-    type: "special",
-    desired: null,
-    options: {
-      "!": true,
-      "@": true,
-      "#": true,
-      //"$": true,
-      "%": true,
-      "^": true,
-      "&": true,
-      "*": true,
-      "(": true,
-      ")": true,
-      "-": true,
-      "=": true,
-      //"_": true,
-      "+": true,
-      "[": true,
-      "]": true,
-      "{": true,
-      "}": true,
-      "|": true,
-      ":": true,
-      ";": true,
-      ",": true,
-      ".": true,
-      "<": true,
-      ">": true,
-      "?": true,
-      "/": true,
-    },
-    generateChar: function () {
-      rndmIndex = Math.floor(Math.random() * updatedSymbolArr.length);
-      return updatedSymbolArr[rndmIndex];
-    },
-  },
-  { length: null },//4
-  { exclude: [] },//5
-];
-
-var pwRandomizerArr = [];
+};
 var updatedSymbolArr = [];
-
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -130,122 +78,99 @@ function writePassword() {
 
 function setPwLength() {
   var pwLengthCheck = false;
-  var pwLength;
+  var desiredLength;
   while (!pwLengthCheck) {
-    pwLength = prompt(
+    desiredLength = prompt(
       "How many characters would you like your password to be? Enter a number between 8 and 128"
     );
-
-    if (pwLength < 8) {
+    if (desiredLength < 8) {
       alert("Password must be atleast 8 characters long");
-    } else if (pwLength > 128) {
+    } else if (desiredLength > 128) {
       alert("Password cannot be longer than 128 characters");
     } else {
       pwLengthCheck = true;
     }
   }
-  return pwLength;
+  return desiredLength;
 }
-
 function setCharTypes(type) {
   return confirm(
     "Would you like your password to include " + type + " characters?"
   );
 }
-
 function generatePassword() {
   var generatedPassword = "";
   defineCriteria();
-  //setRandomizer();
-
-  for (var i = 0; i < pwObj.length; i++) {
-    var rndmIndex = Math.floor(Math.random() * pwRandomizerArr.length);
-    generatedPassword += pwRandomizerArr[rndmIndex];
+  for (var i = 0; i < pwOptions.pwLength; i++) {
+    
+  //generatedPassword.concat(generateCharacter());
+  //generatedPassword.push(generateCharacter());
+  generatedPassword += generateCharacter();
   }
 
   return generatedPassword;
-  //   console.log("generatedPassword: " + generatedPassword)
+//   console.log("generatedPassword: " + generatedPassword)
 }
 
 function defineCriteria() {
-  var pwHasChar = false;
-
   //ask user params for pw
-  pwObj.length = setPwLength();
-
-  while (pwHasChar == false) {
-
-    for (var i = 0; i < 4; i++) {
-        console.log("pwObj[i].type")
-        console.log(pwObj[i].type)
-    pwObj[i].desired = setCharTypes(pwObj[i].type);
- 
-    }
-
-    if (
-      !pwObj[0].desired &&
-      !pwObj[1].desired &&
-      !pwObj[2].desired &&
-      !pwObj[3].desired
-    ) {
-      alert("The Password must have alpha, numeric, or special characters!");
-    } else {
-      pwHasChar = true;
-    }
-  }
-
+  pwOptions.pwLength = setPwLength();
+  pwOptions.lowercase = setCharTypes("lowercase");
+  pwOptions.uppercase = setCharTypes("uppercase");
+  pwOptions.numeric = setCharTypes("numeric");
+  pwOptions.special = setCharTypes("special");
   //if they want symbols, which ones should be excluded
-  if (pwObj[3].desired) {
-    pwObj[5].exclude = prompt(
+  if (pwOptions.special) {
+    pwOptions.exclude = prompt(
       "Which characters should not be included in your password: " +
-      Object.keys(pwObj[3].options));
-    
-  };
-
-  console.log("Exclude: " + pwObj[5].exclude)
+        availableCriteria.symbol
+    );
     //convert to array
-    var tempArr = Object.values(pwObj[5].exclude);
-    var tempArr2 = Object.keys(pwObj[3].options);
+    var tempArr = Object.values(pwOptions.exclude);
+    var tempArr2 = Object.keys(availableCriteria.symbol);
     //loop through excluded symbols obj
     tempArr.forEach((e, i) => {
       //if symbol obj includes excluded symbols array
       if (tempArr2.includes(e)) {
         //change key:value pair from true to false
-        //console.log("pwObj[3].options[e]");
-        console.log(pwObj[3].options[e]);
-        pwObj[3].options[e] = false;
-        console.log(pwObj[3].options[e]);
+        availableCriteria.symbol[e] = false;
       }
     });
-
     //go through sumbol obj
     tempArr2.forEach((e) => {
       //if value of symbol obj's key is true,
-      if (pwObj[3].options[e]) {
+      if (availableCriteria.symbol[e]) {
         //add symbol to new array
         updatedSymbolArr.push(e);
       }
     });
-
-    console.log(pwObj[3])
-    console.log(pwObj[3].desired)
-
-    for ( var i = 0; i < 4; i++) {
-        console.log("wtf");
-        console.log(i)
-        console.log(pwObj[i].desired)
-        // console.log(!Object.values(pwObj[i].desired));
-        // console.log(!pwObj[i]["desired"]);
-        if (!pwObj[i].desired) {
-            // console.log("Entered if statement")
-            // console.log(!pwObj[i].desired)
-            pwObj.splice(i, 1);
-        }
-    }
-
   }
-
+}
+function generateCharacter() {
+  var genChar = "";
+  var index = Math.floor(Math.random() * 3);
+  switch (index) {
+    case 0:
+      var char = Math.floor(Math.random() * 26);
+      var letterCase = Math.floor(Math.random() * 2);
+      if (letterCase) genChar = availableCriteria.aplha[char];
+      else if (!letterCase)
+        genChar = availableCriteria.aplha[char].toLowerCase();
+      else {
+        console.log("error in alpha assignment");
+      }
+      break;
+    case 1:
+      genChar = Math.floor(Math.random() * 10);
+      break;
+    case 2:
+      rndmIndex = Math.floor(Math.random() * updatedSymbolArr.length);
+      genChar = updatedSymbolArr[rndmIndex];
+      break;
+  }
+  console.log("genChar: " + genChar);
+  return genChar
+}
 //passwordText.value = password;
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
