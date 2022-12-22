bd050447d2133ec1e9d023f991d19aec984fc3ed
Troy Johnson <troynjohnson6@gmail.com>
2022-12-21 16:16:30 -0800

    adding logic to specify params of desired password

--- script.js
+++ script.js
@@ -1,48 +1,144 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var pwObject = {
-    length: getPwLength(),
-    lowercase: getCharTypes("lowercase"),
-    uppercase: getCharTypes("uppercase"),
-    numeric: getCharTypes("numeric"),
-    special: getCharTypes("special"),
-}
+var pwObj = {
+  lowercase: null,
+  uppercase: null,
+  numeric: null,
+  special: null,
+  length: null,
+  exclude: null,
+};
 
 // Write password to the #password input
 function writePassword() {
-
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
 }
 
-function getPwLength () {
-    var pwLengthCheck = false;
-    var pwLength;
-    while (!pwLengthCheck) {
-      pwLength = prompt(
-        "How many characters would you like your password to be? Enter a number between 8 and 128"
-      );
-  
-      if (pwLength < 8) {
-        alert("Password must be atleast 8 characters long");
-      } else if (pwLength > 128) {
-        alert("Password cannot be longer than 128 characters");
-      } else {
-          pwLengthCheck = true;
-      }
+function setPwLength() {
+  var pwLengthCheck = false;
+  var pwLength;
+  while (!pwLengthCheck) {
+    pwLength = prompt(
+      "How many characters would you like your password to be? Enter a number between 8 and 128"
+    );
+
+    if (pwLength < 8) {
+      alert("Password must be atleast 8 characters long");
+    } else if (pwLength > 128) {
+      alert("Password cannot be longer than 128 characters");
+    } else {
+      pwLengthCheck = true;
     }
-    return pwLength
+  }
+  return pwLength;
 }
 
-function getCharTypes (type) {
-   return confirm("Would you like your password to inclue " + type + " characters?")
+function setCharTypes(type) {
+  return confirm(
+    "Would you like your password to include " + type + " characters?"
+  );
 }
 
-function generatePassword () {
+function generatePassword() {
+  pwPopulator = {
+    aplha: [
+      "A",
+      "B",
+      "C",
+      "D",
+      "E",
+      "F",
+      "G",
+      "H",
+      "I",
+      "J",
+      "K",
+      "L",
+      "M",
+      "N",
+      "O",
+      "P",
+      "Q",
+      "R",
+      "S",
+      "T",
+      "U",
+      "V",
+      "W",
+      "X",
+      "Y",
+      "Z",
+    ],
+    num: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
+    symbol: {
+      "!": true,
+      "@": true,
+      "#": true,
+      "$": true,
+      "%": true,
+      "^": true,
+      "&": true,
+      "*": true,
+      "(": true,
+      ")": true,
+      "-": true,
+      "=": true,
+      "_": true,
+      "+": true,
+      "[": true,
+      "]": true,
+      "{": true,
+      "}": true,
+      "|": true,
+      ":": true,
+      ";": true,
+      ",": true,
+      ".": true,
+      "<": true,
+      ">": true,
+      "?": true,
+      "/": true,
+    },
+  };
+  //var excludeArr = [];
 
+  pwObj.length = setPwLength();
+  pwObj.lowercase = setCharTypes("lowercase");
+  pwObj.uppercase = setCharTypes("uppercase");
+  pwObj.numeric = setCharTypes("numeric");
+  pwObj.special = setCharTypes("special");
+
+  for (var i = 0; i < pwObj.length; i++) {
+    var charType = Math.floor(Math.random() * 4);
+    pwPopulator;
+  }
+
+  console.log("pwPopulator.symbol");
+    console.log(pwPopulator.symbol["!"]);
+    console.log(pwPopulator.symbol[3]);
+    console.log(pwPopulator.symbol[4]);
+
+  if (pwObj.special) {
+    pwObj.exclude = prompt(
+      "Which characters should not be included in your password: " + pwPopulator.symbol
+    );
+var tempArr = Object.values(pwObj.exclude)
+var tempArr2 = Object.keys(pwPopulator.symbol)
+
+    tempArr.forEach((e, i) => {
+        if (tempArr2.includes(e)) {
+            pwPopulator.symbol[e]=false;
+        }
+        
+    })
+
+    console.log("pwPopulator.symbol");
+    console.log(pwPopulator.symbol);
+  }
 }
 
+//getRndmValue ()
 
 //passwordText.value = password;
 
